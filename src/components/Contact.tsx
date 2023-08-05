5d058d367b5f4a8644687a9d02c0de75bd5ee452
Sipang K <plaaom39@gmail.com>
2023-08-05 22:43:08 +0700

    chore: create Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <section className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-2xl font-bold">Contact</h1>
+    </section>
+  );
+}
+
+export default Contact;
