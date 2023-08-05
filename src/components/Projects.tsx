cf196618819a19565fd6e94bd0cca548328f4cc9
Sipang K <plaaom39@gmail.com>
2023-08-05 22:43:32 +0700

    chore: create Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,14 @@
+function Projects() {
+  return (
+    <section className="flex flex-col items-center h-full pt-5">
+      <h1 className="text-2xl font-bold">My Projects</h1>
+      <div className="flex justify-evenly w-[20rem] xl:w-[30rem] bg-green-100 ">
+        <p>#Full-Stack</p>
+        <p>#Back-end</p>
+        <p>#Front-end</p>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
