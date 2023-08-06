5dfb5d2d15e208a3cd3f56506afc7384b6b26da8
Sipang K <plaaom39@gmail.com>
2023-08-06 21:10:03 +0700

    chore(profile): update profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,27 +1,48 @@
 // data
 import { skills_lang, skills_frame, skills_tools } from "../utils/skills";
+import { BiSolidMap } from "react-icons/bi";
+import { IoSchoolSharp } from "react-icons/io5";
+import { PiCertificateFill } from "react-icons/pi";
+import { FaLightbulb } from "react-icons/fa";
 
 function Profile() {
   return (
-    <section id="profile" className="flex flex-col items-center h-auto xl:h-full pt-5 bg-red-200">
+    <section id="profile" className="flex flex-col items-center h-auto xl:h-full pt-5">
       <h1 className="text-2xl font-bold">Profile</h1>
       <div className="grid grid-rows-2 grid-cols-1 h-full w-full">
-        <div className="grid grid-cols-1 xl:grid-cols-3 bg-red-400">
-          <div className="flex justify-center items-center">Live</div>
-          <div className="flex justify-center items-center">Education</div>
-          <div className="flex justify-center items-center">Certificate</div>
+        <div className="grid grid-cols-1 xl:grid-cols-3">
+          <div className="flex flex-col justify-center items-center p-5">
+            <BiSolidMap className="w-[30px] h-[30px]" />
+            <h2 className="text-lg font-semibold mb-5">Live</h2>
+            Pathum Thani, Thailand
+          </div>
+          <div className="flex flex-col justify-center items-center text-center p-5">
+            <IoSchoolSharp className="w-[30px] h-[30px]" />
+            <h2 className="text-lg font-semibold mb-5">Education</h2>
+            <h1>King Mongkut's Institute of Technology Ladkrabang</h1>
+            <h4>Bachelor of Science - Information Technology</h4>
+            <p>(Software Engineer)</p>
+            <p>Complete 2023</p>
+          </div>
+          <div className="flex flex-col justify-center items-center text-center p-5">
+            <PiCertificateFill className="w-[30px] h-[30px]" />
+            <h2 className="text-lg font-semibold mb-5">Certificate</h2>
+            <h1>React Real-World Projects (Udemy)</h1>
+            <h1>TypeScript from Basic to Advanced (Udemy)</h1>
+          </div>
         </div>
-        <div className="flex flex-col justify-center items-center bg-green-400">
-          Skills
+        <div className="flex flex-col justify-center items-center text-center">
+          <FaLightbulb className="w-[30px] h-[30px]" />
+          <h2 className="text-lg font-semibold mb-5">Skills</h2>
           <div className="grid grid-cols-1 xl:grid-cols-3">
             <div>
-              <h1 className="text-center">Programming Language</h1>
-              <div className="flex justify-center flex-wrap bg-pink-400 p-5">
+              <h1 className="text-center font-medium">Programming Language</h1>
+              <div className="flex justify-center flex-wrap p-5">
                 {skills_lang.map((skill, index) => {
                   const htmlText = skill.icon;
                   return (
                     <div
-                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-white p-[.3rem] w-fit hover:bg-slate-300 m-1"
+                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-slate-100 p-[.3rem] w-fit hover:bg-slate-300 m-1"
                       key={index}
                     >
                       <div
@@ -35,13 +56,13 @@ function Profile() {
               </div>
             </div>
             <div>
-              <h1 className="text-center">Framework & Library</h1>
-              <div className="flex justify-center flex-wrap bg-purple-400 p-5">
+              <h1 className="text-center font-medium">Framework & Library</h1>
+              <div className="flex justify-center flex-wrap p-5">
                 {skills_frame.map((skill, index) => {
                   const htmlText = skill.icon;
                   return (
                     <div
-                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-white p-[.3rem] w-fit hover:bg-slate-300 m-1"
+                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-slate-100 p-[.3rem] w-fit hover:bg-slate-300 m-1"
                       key={index}
                     >
                       <div
@@ -55,13 +76,13 @@ function Profile() {
               </div>
             </div>
             <div>
-              <h1 className="text-center">Tools</h1>
-              <div className="flex justify-center flex-wrap bg-orange-400 p-5">
+              <h1 className="text-center font-medium">Tools</h1>
+              <div className="flex justify-center flex-wrap p-5">
                 {skills_tools.map((skill, index) => {
                   const htmlText = skill.icon;
                   return (
                     <div
-                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-white p-[.3rem] w-fit hover:bg-slate-300 m-1"
+                      className="flex items-center rounded-lg transition-all duration-300 ease-out bg-slate-100 p-[.3rem] w-fit hover:bg-slate-300 m-1"
                       key={index}
                     >
                       <div
