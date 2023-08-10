e141fbe3a471965b91f0a57adeedf91ae6397585
Sipang K <plaaom39@gmail.com>
2023-08-11 00:53:53 +0700

    chore: add ActiveNavbarProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../src/styles/index.css";
+import { ActiveNavbarProvider } from "./context/activeSectionNavbar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ActiveNavbarProvider>
+      <App />
+    </ActiveNavbarProvider>
   </React.StrictMode>
 );
