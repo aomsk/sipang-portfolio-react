9617f0796359e8d696f54da7b5c83daf002492ec
Sipang K <plaaom39@gmail.com>
2023-08-11 00:48:59 +0700

    add: add new data and change data file name

---
+++ src/utils/data.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    title: "Home",
+    link: "#",
+  },
+  {
+    title: "Profile",
+    link: "#profile",
+  },
+  {
+    title: "My Projects",
+    link: "#projects",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
+];
