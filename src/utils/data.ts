168b35d6056c9666a71927da1629a4d780c4bfa2
Sipang K <plaaom39@gmail.com>
2023-08-19 11:42:17 +0700

    update tags data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -24,7 +24,7 @@ export const links: ILinks[] = [
   // },
 ];
 
-export const tags: string[] = ["All", "FullStack", "Backend", "Frontend", "Mobile"];
+export const tags: string[] = ["All", "FullStack", "Frontend", "Mobile"];
 
 export const certificates: ICertificate[] = [
   {
